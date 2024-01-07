e0728c4db332c0bbb4dfd78b4e19ec8b86ac210e
Viktor <Verkhorubovvv@mail.ru>
2024-01-07 20:00:40 +0800

    созданы методы

--- Desktop/контрольная/Program.cs
+++ Desktop/контрольная/Program.cs
@@ -2,3 +2,46 @@
 //либо равна 3 символам. Первоначальный массив можно ввести с клавиатуры, либо задать на старте выполнения алгоритма. 
 //При решении не рекомендуется пользоваться коллекциями, лучше обойтись исключительно массивами.
 
+   //метод для разделения строки в массив
+string[] GetArrayStringConsole (string inConsolSimvol)
+{
+    string[] arraySimvol = new string[inConsolSimvol.Length];
+    arraySimvol = inConsolSimvol.Split(",");
+    return arraySimvol;
+}
+
+    //метод сортирующий символы из массива
+string[] GetArrayThreeSimvol (string[] array)
+{
+    int count = 0;
+    for (int i = 0; i < array.Length; i++)
+    {
+        string simvol = array[i];
+        if (simvol.Length <= 3)
+        {
+            count++;
+        }
+    }
+    string[] arrayThreeSimvol = new string[count];
+    for (int i = 0, j = 0; i < array.Length; i++)
+    {
+        string simvol = array[i];
+        if (simvol.Length <= 3)
+        {
+            arrayThreeSimvol[j] = simvol;
+            j++;
+        }
+    }
+    return arrayThreeSimvol;
+}
+
+
+       //метод для вывода информации из массивов
+void Print (string[] array)
+{
+    for (int i = 0; i < array.Length; i++)
+    {
+        Console.Write($"{array[i]}, ");
+    }
+    Console.WriteLine();
+}
